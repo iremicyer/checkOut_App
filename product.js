0b481c7850b3cd004aa3b222881654ec3fcceac5
iremicyer <iremicyer@gmail.com>
2022-02-24 11:50:50 +0300

    commits

---
+++ product.js
@@ -0,0 +1,10 @@
+class Product {
+    constructor(productName, image, price, oldPrice, quantity, productTotal) {
+        this.productName = productName;
+        this.image = image;
+        this.price = price;
+        this.oldPrice = oldPrice;
+        this.quantity = quantity;
+        this.productTotal = productTotal;
+    }
+}
